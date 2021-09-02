32a3ccde3879dc8c1635d41f76e788e1ed920c6b
avinash-cs <avinashsingh880086@gmail.com>
2021-09-02 19:09:24 +0530

    new

--- client/src/components/body/welcome/welcome.js
+++ client/src/components/body/welcome/welcome.js
@@ -1,14 +1,12 @@
 import React from 'react'
 // import {Link} from 'react-router-dom'
 
-import './welcome.css'
 
 function Welcome() {
     return (
        
             <div>
 
-           
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
              <div className="container-fluid">
                  <a className="navbar-brand" href="/">Available</a>
