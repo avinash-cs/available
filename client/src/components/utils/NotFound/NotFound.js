1aeb3e9771e6e00385a90ab8b82c9e44c05d962f
avinash-cs <avinashsingh880086@gmail.com>
2021-11-08 18:45:51 +0530

    1st point access denied done

--- client/src/components/utils/NotFound/NotFound.js
+++ client/src/components/utils/NotFound/NotFound.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import './notfound.css'
+import Header from '../../header/Header'
 function NotFound() {
     return (
         <div>
-            404 | Not Found
-            <button><a href="/">Home</a></button>
+        <Header />
+        <div class="not-found-main">
+            <h1> 404 || Not Found </h1>
+            <h4>Try Again , by loging in or loging out</h4>
+            <br  />
+            <button id="a-notfound" class="btn btn-outline-success my-2 my-sm-0" ><a  href="/">Home</a></button>
+        </div>
         </div>
     )
 }
