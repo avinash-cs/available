75ae1373935d2d5dd286ff4dae31233c4a829d31
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-16 13:18:55 +0530

    Added info of Indian states(districts, cities) in setup profile

--- client/src/components/body/setupprofile/setupprofile.js
+++ client/src/components/body/setupprofile/setupprofile.js
@@ -1,49 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../header/Header'
 import './setupprofile.css';
+import {towns, states} from './data';
 
 function Setupprofile() {
-    var stateObject = {
-"Haryana": { "Gurugram": ["new Delhi", "North Delhi"],
-"Faridabad": ["Thiruvananthapuram", "Palakkad"],
-"Goa": ["North Goa", "South Goa"],
-},
-"Delhi": {
-"South Australia": ["Dunstan", "Mitchell"],
-"Victoria": ["Altona", "Euroa"]
-}, "Rajasthan": {
-"Alberta": ["Acadia", "Bighorn"],
-"Columbia": ["Washington", ""]
-},
-}
-window.onload = function () {
-var countySel = document.getElementById("countySel"),
-stateSel = document.getElementById("stateSel"),
-districtSel = document.getElementById("districtSel");
-for (var country in stateObject) {
-countySel.options[countySel.options.length] = new Option(country, country);
-}
-countySel.onchange = function () {
-stateSel.length = 1; // remove all options bar first
-districtSel.length = 1; // remove all options bar first
-if (this.selectedIndex < 1) return; // done
-for (var state in stateObject[this.value]) {
-stateSel.options[stateSel.options.length] = new Option(state, state);
-}
-}
-countySel.onchange(); // reset in case page is reloaded
-stateSel.onchange = function () {
-districtSel.length = 1; // remove all options bar first
-if (this.selectedIndex < 1) return; // done
-var district = stateObject[countySel.value][this.value];
-for (var i = 0; i < district.length; i++) {
-districtSel.options[districtSel.options.length] = new Option(district[i], district[i]);
-}
-}
-}
-    
-    
-    
+    const [district, setDistrict] = useState("");
+    const [city, setCity] = useState("");
+
+    useEffect(() => {
+        const stateSelect = document.getElementById('stateSel');
+        const indianStates = states.states;
+        indianStates.forEach(state => {
+            const option = document.createElement('option');
+            const name = state.state;
+            option.text = name;
+            stateSelect.appendChild(option);
+        })
+    }, [towns, states]);
+
+    // state change handler
+    const stateChangeHandler = (e) => {
+        console.log('State changed to', e.target.value);
+        const indianStates = states.states;
+        const districtSel = document.getElementById('districtSel');
+        indianStates.forEach(state => {
+            const name = state.state;
+            if (name === 'Haryana') {
+                const districts = state.districts;
+                districts.forEach(district => {
+                    const option = document.createElement('option');
+                    option.text = district;
+                    districtSel.appendChild(option);
+                })
+            }
+        });
+    }
+
+    // remove options helper
+    const removeOptions = (selectElement) => {
+        let i, L = selectElement.options.length - 1;
+        for(i = L; i >= 0; i--) {
+           selectElement.remove(i);
+        }
+    }
+
+    // district change handler
+    const districtChangeHandler = (e) => {
+        console.log('District changed to', e.target.value);
+        const districtChosen = e.target.value;
+        setDistrict(districtChosen);
+
+        const townSelect = document.getElementById('townSel');
+
+        // removing the previous options
+        removeOptions(townSelect);
+        
+        const defaultOption = document.createElement('option');
+        defaultOption.text = 'Select your city/village';
+        townSelect.appendChild(defaultOption);
+
+        if (districtChosen in towns) {
+            const townsInDistrict = towns[districtChosen];
+
+            townsInDistrict.forEach(town => {
+                const option = document.createElement('option');
+                option.text = town;
+                townSelect.appendChild(option);
+            })
+        } else {
+            const option = document.createElement('option');
+            option.text = 'DEFAULT TOWN';
+            townSelect.appendChild(option);
+        }
+    }
+
+    // city change handler
+    const cityChangeHandler = (e) => {
+        console.log('City name changed to', e.target.value);
+    } 
+
     return (
         <div>
         <Header />
@@ -60,22 +95,23 @@ districtSel.options[districtSel.options.length] = new Option(district[i], distri
                         <option>select</option>
                         <option value="male">Male</option>
                         <option value="female">Female</option>
+                        <option value="other">Other</option>
 
                         </select>
                         <br />
                         <br />
-                        Select State: <select name="state" id="countySel" size="1">
+                        Select State: <select name="state" id="stateSel" size="1" onChange={stateChangeHandler}>
                             <option value="" selected="selected">Select State</option>
                             </select>
                             <br />
                             <br />
-                            Select District: <select name="countrya" id="stateSel" size="1">
-                            <option value="" selected="selected">Please select State first</option>
+                            Select District: <select name="district" id="districtSel" size="1" onChange={districtChangeHandler}>
+                            <option value="" selected="selected">Select your district</option>
                             </select>
                             <br />
                             <br />
-                            Select Village/City Name: <select name="district" id="districtSel" size="1">
-                            <option value="" selected="selected">Please select District first</option>
+                            Select Village/City Name: <select name="city" id="townSel" size="1" onChange={cityChangeHandler}>
+                            <option value="" selected="selected">Select your city/village</option>
                             </select><br />
                             <br />
 
